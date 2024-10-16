a715e95bf1e3d562d2d056f23aa48beeaa4434a9
Mokshada2721 <sablemokshada@gmail.com>
2024-10-17 04:06:19 +0530

    Added terraform files

---
+++ main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+}
+
+provider "docker" {}
+
+# Pull Docker images from Docker Hub
+data "docker_image" "backend_image" {
+  name = "mokshada21/devops-blog:backend-latest"
+}
+
+data "docker_image" "frontend_image" {
+  name = "mokshada21/devops-blog:frontend-latest"
+}
+
+# Backend container with environment variables
+resource "docker_container" "backend_container" {
+  name  = "backend_app"
+  image = data.docker_image.backend_image.name
+
+  ports {
+    internal = 5000
+    external = 5000
+  }
+
+  env = [
+    "MONGODB_URI=${var.MONGODB_URI}",
+    "TOKEN_KEY=${var.TOKEN_KEY}",
+    "EMAIL=${var.EMAIL}",
+    "PASSWORD=${var.PASSWORD}"
+  ]
+}
+
+# Frontend container
+resource "docker_container" "frontend_container" {
+  name  = "frontend_app"
+  image = data.docker_image.frontend_image.name
+
+  ports {
+    internal = 3000
+    external = 3000
+  }
+}
+
+# Wait for containers to start
+resource "null_resource" "wait_for_containers" {
+  depends_on = [docker_container.backend_container, docker_container.frontend_container]
+
+  provisioner "local-exec" {
+    command = "powershell -Command Start-Sleep -Seconds 10"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "MONGODB_URI" {
+  description = "MongoDB connection string"
+  type        = string
+}
+
+variable "TOKEN_KEY" {
+  description = "Token key for authentication"
+  type        = string
+}
+
+variable "EMAIL" {
+  description = "Email address"
+  type        = string
+}
+
+variable "PASSWORD" {
+  description = "Password for the backend service"
+  type        = string
+}
